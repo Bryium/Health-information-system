8bcfe65ae484b3c2d2d4bca4ef6a5ba9def4aa27
Bryium <bryiumonyancha@gmail.com>
2025-04-25 04:13:46 +0300

    add view_client file

--- routes.py
+++ routes.py
@@ -72,17 +72,17 @@ def view_client(client_id):
 @main_bp.route('/search-client', methods=['GET', 'POST'])
 def search_client():
     if request.method == 'POST':
-        # Get the client name from the form
+        
         client_name = request.form.get('client_name')
 
-        # Query the database to find the client by name
+        
         client = Client.query.filter(Client.client_name.ilike(f'%{client_name}%')).first()
 
-        # If a client is found, redirect to their profile page
+        
         if client:
             return redirect(url_for('main.view_client', client_id=client.id))
 
-        # If no client is found, show a message (optional)
+        
         flash('Client not found!', 'danger')
         return redirect(url_for('main.search_client'))
 
